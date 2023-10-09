3c49ec5cb3a4799a49af4627a18136e9d737c19b
TheSaneWriter <93878758+TheSaneWriter@users.noreply.github.com>
2023-10-09 14:34:36 -0400

    Lemmy 0.19 Support

--- src/index.js
+++ src/index.js
@@ -135,6 +135,7 @@ async function main() {
         password: localPassword,
       };
       let user = await localClient.login(loginForm);
+      localClient.setHeaders({Authorization: "Bearer " + user.jwt});
       for await (const remoteInstance of remoteInstances) {
         try {
           for await (const communitySortMethod of communitySortMethods) {
